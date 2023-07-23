b4e4d185ff41740e756f7182b404fc83224e00d8
Weilin Wang <littrumpe@weilins-mbp.home>
2023-07-23 12:50:30 +0100

    Support chests and path tracking

--- kenney_exploration/src/game.ts
+++ kenney_exploration/src/game.ts
@@ -4,23 +4,34 @@ import { Tiles } from "./tiles";
 import village from "../map/village.json";
 
 function toMapKey({ x, y }: { x: number; y: number }) {
-  return `${x}:x${y}`;
+  return `${x}:${y}`;
 }
+
+const FOREST_LEFT_X = 19;
+const DUNGEON_LEFT_X = 138;
 
 function loadWorld() {
   console.log("village", village);
   const tileNames = new Map<number, string>();
+  const monsterNames = new Map<number, string>();
   for (const ts of village.tilesets) {
     for (const t of ts.tiles) {
       const id = ts.firstgid + t.id;
-      let name = undefined;
+      let tileName = undefined;
+      let monsterName = undefined;
       for (const p of t.properties) {
         if (p.name === "name" && p.value !== "") {
-          name = p.value;
+          tileName = p.value;
+        }
+        if (p.name === "monster_name" && p.value !== "") {
+          monsterName = p.value;
         }
       }
-      if (name !== undefined) {
-        tileNames.set(id, name);
+      if (tileName !== undefined) {
+        tileNames.set(id, tileName);
+      }
+      if (monsterName !== undefined) {
+        monsterNames.set(id, monsterName);
       }
     }
   }
@@ -35,6 +46,8 @@ function loadWorld() {
         name?: string | undefined;
         drawBeforeBuildings?: boolean;
         teleporterName?: string | undefined;
+        monsterName?: string | undefined;
+        chestName?: string | undefined;
       }
     >();
     const layer = village.layers[layerIdx];
@@ -42,12 +55,18 @@ function loadWorld() {
       case "objectgroup":
         for (const obj of layer.objects ?? []) {
           const gid = obj.gid;
-          const name = tileNames.get(gid);
+          const baseGid = gid % (1 << 29);
+          const name = tileNames.get(baseGid);
+          const monsterName = monsterNames.get(baseGid);
+          let chestName: string | undefined = undefined;
           let drawBeforeBuildings = false;
           let teleporterName = undefined;
           for (const p of obj.properties ?? []) {
-            if (p.name === "chest" && p.value === "forest_hidden_chest") {
-              drawBeforeBuildings = true;
+            if (p.name === "chest") {
+              chestName = p.value;
+              if (p.value === "forest_hidden_chest") {
+                drawBeforeBuildings = true;
+              }
             }
             if (p.name === "stairs") {
               teleporterName = p.value;
@@ -62,6 +81,8 @@ function loadWorld() {
             name,
             drawBeforeBuildings,
             teleporterName,
+            monsterName,
+            chestName,
           });
         }
         break;
@@ -98,7 +119,8 @@ export const runGame = ({
   tiles: Tiles;
 }) => {
   const world = loadWorld();
-  const player = { x: 0, y: 0 };
+  const openedChests = new Set<string>();
+  const player = { x: 0, y: 0, sightRadius: 4 };
   let playerKey;
   for (const [key, obj] of world.items) {
     if (obj.name === "player") {
@@ -107,9 +129,24 @@ export const runGame = ({
       playerKey = key;
     }
   }
+  // TODO debug
+  player.x = -2;
+  player.y = 11;
   if (playerKey !== undefined) {
     world.items.delete(playerKey);
   }
+  let mazeStringStack: {
+    from: { x: number; y: number };
+    to: { x: number; y: number };
+  }[] = [];
+  const mazeStringSet = new Set<string>();
+  function recomputeMazeStringSet() {
+    mazeStringSet.clear();
+    for (const p of mazeStringStack) {
+      mazeStringSet.add(toMapKey(p.to));
+    }
+  }
+  let foundMazeString = false;
   //   const coinAudio = new Audio("./chiptone/coin.wav");
   const ctx = canvas;
   ctx.imageSmoothingEnabled = false;
@@ -184,14 +221,53 @@ export const runGame = ({
       }
     }
     ctx.filter = "none";
+    ctx.strokeStyle = "gold";
+    ctx.lineWidth = 3.0;
+    if (foundMazeString) {
+      ctx.beginPath();
+      for (const step of mazeStringStack) {
+        ctx.moveTo(
+          128 + (step.from.x - player.x) * 16,
+          96 + (step.from.y - player.y) * 16
+        );
+        ctx.lineTo(
+          128 + (step.to.x - player.x) * 16,
+          96 + (step.to.y - player.y) * 16
+        );
+      }
+      ctx.stroke();
+    }
+
     tiles.drawOn(ctx, {
       image: 19 * 12 + 4,
       x: 128 - 8,
       y: 96 - 8,
       flipHorizontally: playerFacing === "left",
     });
+
+    // 6*6 + 8*8
+    // 36 + 64
+    // 100
+
+    // 4*4 ==> 16
+    if (player.x >= DUNGEON_LEFT_X) {
+      for (let dy = -6 * 16; dy <= 6 * 16; dy += 1) {
+        for (let dx = -8 * 16; dx <= 8 * 16; dx += 1) {
+          const dist = (dy * dy + dx * dx) / 16 / 16;
+          if (dist >= player.sightRadius) {
+            ctx.fillStyle = `rgba(0,0,0,1)`;
+          } else {
+            ctx.fillStyle = `rgba(0,0,0,${dist / player.sightRadius})`;
+          }
+          ctx.fillRect(dx + 128, dy + 96, 1, 1);
+        }
+      }
+    }
+
     ctx.fillStyle = "blue";
     ctx.fillText(`${Math.floor(time)}ms`, 5, 20);
+    ctx.fillText(`x=${player.x} y=${player.y}`, 5, 40);
+    ctx.fillText(`maze stack size ${mazeStringStack.length}`, 5, 60);
     requestAnimationFrame(redraw);
   }
   requestAnimationFrame(redraw);
@@ -206,11 +282,20 @@ export const runGame = ({
     if (world.map.get(toMapKey(to)) === undefined) {
       return;
     }
+    const item = world.items.get(toMapKey(to));
+    if (item?.monsterName !== undefined) {
+      alert(`The ${item.monsterName} blocks your path!`);
+      return;
+    }
+    if (item?.chestName !== undefined) {
+      alert(`Opened chest: ${item.chestName}`);
+      item.gid += 2;
+      item.chestName = undefined;
+    }
     if (world.buildings.get(toMapKey(to)) !== undefined) {
       return;
     }
     function maybe_teleport() {
-      const item = world.items.get(toMapKey(to));
       if (item && item.teleporterName !== undefined) {
         console.log("stepped on", item);
         for (const obj of world.items.values()) {
@@ -232,9 +317,41 @@ export const runGame = ({
     if (teleportTo) {
       to = teleportTo;
     }
+    if (foundMazeString) {
+      const seen = mazeStringSet.has(toMapKey(to));
+      if (seen) {
+        while (true) {
+          const removed = mazeStringStack.pop();
+          if (!removed) break;
+          if (toMapKey(removed.from) === toMapKey(to)) {
+            break;
+          }
+        }
+        recomputeMazeStringSet();
+      } else {
+        mazeStringStack.push({
+          from: { x: player.x, y: player.y },
+          to: { x: to.x, y: to.y },
+        });
+        mazeStringSet.add(toMapKey(to));
+      }
+    }
     player.x = to.x;
     player.y = to.y;
-    // redraw();
+    if (player.x === 178 && player.y === 12 && dx === 1) {
+      foundMazeString = true;
+      player.sightRadius *= 2;
+      mazeStringStack = [
+        {
+          from: { x: player.x, y: player.y },
+          to: { x: player.x, y: player.y },
+        },
+      ];
+      recomputeMazeStringSet();
+    }
+    if (player.x === 177 && player.y === 12) {
+      foundMazeString = false;
+    }
   }
   keyboard.onKeyDown("ArrowLeft", () => {
     goto(-1, 0);
