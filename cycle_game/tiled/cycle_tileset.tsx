696ddba1443392c327bd30f5d9925b48ed6bf6d3
Tor Gunnar Høst Houeland <887395+houeland@users.noreply.github.com>
2023-08-04 18:38:07 +0100

    Cropped background images

---
+++ cycle_game/tiled/cycle_tileset.tsx
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="cycle_tileset" tilewidth="300" tileheight="300" tilecount="35" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="50" height="50" source="../../circular_game/baby theme/candy_with_paper.png"/>
+ </tile>
+ <tile id="1">
+  <image width="50" height="50" source="../../circular_game/baby theme/climbing baby_left.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="../../circular_game/baby theme/milk_bottle.png"/>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="../../circular_game/baby theme/parents_love.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="../../circular_game/baby theme/poka_dot_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="../../circular_game/baby theme/poka_dot_2.png"/>
+ </tile>
+ <tile id="6">
+  <image width="50" height="50" source="../../circular_game/baby theme/rocking_horse.png"/>
+ </tile>
+ <tile id="7">
+  <image width="50" height="50" source="../../circular_game/baby theme/teddy_bear.png"/>
+ </tile>
+ <tile id="8">
+  <image width="50" height="50" source="../../circular_game/baby theme/background/blue_circle_tile.png"/>
+ </tile>
+ <tile id="9">
+  <image width="300" height="300" source="../../circular_game/baby theme/background/blue_square_corner.png"/>
+ </tile>
+ <tile id="10">
+  <image width="50" height="50" source="../../circular_game/baby theme/background/frame.png"/>
+ </tile>
+ <tile id="11">
+  <image width="50" height="50" source="../../circular_game/baby theme/background/green_circle_tile.png"/>
+ </tile>
+ <tile id="12">
+  <image width="300" height="300" source="../../circular_game/baby theme/background/green_square_corner.png"/>
+ </tile>
+ <tile id="13">
+  <image width="50" height="50" source="../../circular_game/baby theme/background/red_circle_tile.png"/>
+ </tile>
+ <tile id="14">
+  <image width="300" height="300" source="../../circular_game/baby theme/background/red_square_corner.png"/>
+ </tile>
+ <tile id="15">
+  <image width="150" height="150" source="../../circular_game/baby theme/background/square_center.png"/>
+ </tile>
+ <tile id="16">
+  <image width="50" height="50" source="../../circular_game/baby theme/background/yellow_circle_tile.png"/>
+ </tile>
+ <tile id="17">
+  <image width="300" height="300" source="../../circular_game/baby theme/background/yellow_square_corner.png"/>
+ </tile>
+ <tile id="18">
+  <image width="50" height="50" source="../../circular_game/student theme/game_boy.png"/>
+ </tile>
+ <tile id="19">
+  <image width="50" height="50" source="../../circular_game/student theme/graduation_hat.png"/>
+ </tile>
+ <tile id="20">
+  <image width="50" height="50" source="../../circular_game/student theme/love_letter.png"/>
+ </tile>
+ <tile id="21">
+  <image width="50" height="50" source="../../circular_game/student theme/skateboard.png"/>
+ </tile>
+ <tile id="22">
+  <image width="50" height="50" source="../../circular_game/student theme/student 2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="50" height="50" source="../../circular_game/student theme/student.png"/>
+ </tile>
+ <tile id="24">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_bottom_left_corner.png"/>
+ </tile>
+ <tile id="25">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_bottom_left_corner(2).png"/>
+ </tile>
+ <tile id="26">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_bottom_right.png"/>
+ </tile>
+ <tile id="27">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_bottom_right(2).png"/>
+ </tile>
+ <tile id="28">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_top_left_corner.png"/>
+ </tile>
+ <tile id="29">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_top_left_corner(2).png"/>
+ </tile>
+ <tile id="30">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/maze_verticle.png"/>
+ </tile>
+ <tile id="31">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/Sprite-0100.png"/>
+ </tile>
+ <tile id="32">
+  <image width="50" height="50" source="../../circular_game/student theme/student_background/Sprite-0101.png"/>
+ </tile>
+ <tile id="33">
+  <image width="300" height="300" source="../../circular_game/student theme/student_background/student_background.png"/>
+ </tile>
+ <tile id="34">
+  <image width="50" height="50" source="../../circular_game/trophy.png"/>
+ </tile>
+</tileset>
---
+++ cycle_game/tiled/cycle_tileset_old.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="cycle_tileset" tilewidth="50" tileheight="50" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/butterfly.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/climbing baby_left.png"/>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/poka_dot_1.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/poka_dot_2.png"/>
+ </tile>
+ <tile id="5">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/rocking_horse.png"/>
+ </tile>
+ <tile id="6">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/candy_with_paper.png"/>
+ </tile>
+ <tile id="7">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/milk_bottle.png"/>
+ </tile>
+ <tile id="8">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/parents_love.png"/>
+ </tile>
+ <tile id="9">
+  <image width="50" height="50" source="../../../../../Downloads/circular_game/baby theme/teddy_bear.png"/>
+ </tile>
+</tileset>
